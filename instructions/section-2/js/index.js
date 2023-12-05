6460a29c6f8f2a08b0a307e414b67d321efd60b1
Edith Harrison <edithharrison@ediths-laptop.lan>
2023-12-04 20:43:06 -0500

    Added a Message Form

---
+++ instructions/section-2/js/index.js
@@ -0,0 +1,58 @@
+// Insert Copyright Text in Footer
+var today = new Date();
+var thisYear = today.getFullYear();
+var footer = document.querySelector('footer');
+var copyright = document.createElement('p');
+copyright.innerHTML = 'Edith Harrison ' + thisYear;
+footer.appendChild(copyright);
+
+//Create List of Skills
+var skills = ["HTML", "CSS", "JavaScript"];
+var skillsSection = document.getElementById('skills');
+var skillsList = skillsSection.querySelector('ul');
+for (var i = 0; i < skills.length; i++) {
+    var skill = document.createElement('li');
+    skill.innerText = skills[i];
+    skillsList.appendChild(skill);
+}
+
+// Handle Message Form Submit
+const messageForm = document.forms['leave_message'];
+
+messageForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    console.log('Name:', usersName);
+    console.log('Email Address:', usersEmail);
+    console.log('Message:', usersMessage);
+
+    messageForm.reset();
+
+    // Display Messages in List
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+
+    const newMessage = document.createElement('li');
+
+    newMessage.innerHTML = `
+    <a href="mailto:${usersEmail}">${usersName}</a>
+    <span>${usersMessage}</span>
+`;
+    const removeButton = document.createElement('button');
+
+    removeButton.innerText = 'Remove';
+    removeButton.type = 'button';
+
+    removeButton.addEventListener('click', function () {
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+
+    newMessage.appendChild(removeButton);
+
+    messageList.appendChild(newMessage);
+});
