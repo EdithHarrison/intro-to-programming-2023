f87ccb9dee0178458981cd9db512babf9330fbd8
Edith Harrison <edithharrison@ediths-laptop.lan>
2024-01-05 12:54:50 -0500

    AJAX Basic

--- instructions/section-2/js/index.js
+++ instructions/section-2/js/index.js
@@ -7,7 +7,7 @@ copyright.innerHTML = `&copy; Edith Harrison ${thisYear}`;
 footer.appendChild(copyright);
 
 // Create List of Skills
-const skills = ["HTML", "CSS", "JavaScript", "Canva Design", "VSCode"];
+const skills = ["HTML", "CSS", "JavaScript", "AJAX", "VSCode"];
 const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul');
 
@@ -43,6 +43,31 @@ messageForm.addEventListener('submit', function (event) {
         <span>${usersMessage}</span>
     `;
 
+    // Edit Button
+    const editButton = document.createElement('button');
+    editButton.innerText = 'Edit';
+    editButton.type = 'button'; 
+
+    editButton.addEventListener('click', function () {
+        const spanElement = newMessage.querySelector('span');
+        const existingContent = spanElement.textContent;
+
+        const editInput = document.createElement('input');
+        editInput.type = 'text';
+        editInput.value = existingContent;
+
+        newMessage.replaceChild(editInput, spanElement);
+
+        editInput.focus();
+
+        editInput.addEventListener('blur', function () {
+            const editedContent = editInput.value;
+            spanElement.textContent = editedContent;
+            newMessage.replaceChild(spanElement, editInput);
+        });
+    });
+
+    // Remove Button
     const removeButton = document.createElement('button');
     removeButton.innerText = 'Remove';
     removeButton.type = 'button';
@@ -52,6 +77,52 @@ messageForm.addEventListener('submit', function (event) {
         entry.remove();
     });
 
+    newMessage.appendChild(editButton);
     newMessage.appendChild(removeButton);
     messageList.appendChild(newMessage);
 });
+  
+    const githubUsername = "EdithHarrison";
+
+    const githubRequest = new XMLHttpRequest();
+
+    const githubApiUrl = `https://api.github.com/users/${githubUsername}/repos`;
+    githubRequest.open('GET', githubApiUrl);
+
+    githubRequest.onreadystatechange = function () {
+    if (githubRequest.readyState === 4 && githubRequest.status === 200) {
+
+        const repositories = JSON.parse(githubRequest.responseText);
+        console.log(repositories); 
+
+        const projectSection = document.getElementById('projects');
+        const projectList = projectSection.querySelector('ul');
+
+        // Loop through repositories and create <a> tags with additional information
+        for (let i = 0; i < repositories.length; i++) {
+            const project = document.createElement('li');
+            const projectLink = document.createElement('a');
+            const projectDetails = document.createElement('p');
+
+            // Set the href attribute to the GitHub repository URL
+            projectLink.href = repositories[i].html_url;
+
+            // Set the inner text of the <a> tag to the repository name
+            projectLink.innerText = repositories[i].name;
+
+            // Add additional information to the <p> tag
+            projectDetails.innerHTML = `<strong>Description:</strong> ${repositories[i].description || 'N/A'}<br>
+                                        <strong>Created at:</strong> ${new Date(repositories[i].created_at).toLocaleDateString()}`;
+
+            project.appendChild(projectLink);
+            project.appendChild(projectDetails);
+            projectList.appendChild(project);
+        }
+    }
+};
+
+githubRequest.addEventListener('load', function (event) {
+   
+});
+
+githubRequest.send();
